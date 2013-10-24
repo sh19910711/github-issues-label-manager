a4f9de01506b27762f26464a0174be317fe0328b
Hiroyuki Sano <sh19910711@gmail.com>
2013-10-24 10:08:53 +0900

    amend

--- spec/lib/server/t003_user_api_spec.rb
+++ spec/lib/server/t003_user_api_spec.rb
@@ -6,14 +6,23 @@ describe "T003: User API" do
   end
 
   describe "001: Label" do
-    it "001: Create" do
-      puts "Hello create"
+    describe "001: Create" do
+      before {
+        @env = {
+          "rack.session" => {}
+        }
+        Rack::Csrf.new @env
+        post "/api/label"
+      }
+      it "001: Create" do
+        p last_request.env['rack.session']
+      end
     end
 
     describe "002: Read" do
       before { get "/api/label/#{CGI.escape("user/repo/label1")}" }
       subject { JSON.parse(last_response.body)["color"] }
-      it "002: Read" do
+      it "001: Read" do
         should eq "EEEEEE"
       end
     end
